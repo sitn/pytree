9ec1db83d47cd1b39a8a9b8f9f20b455dd72c20b
SITNService <olivier.monod@ne.ch>
2017-10-03 09:00:47 +0200

    remove dev stuff

--- pytree/logging.py
+++ pytree/logging.py
@@ -4,22 +4,24 @@ import os
 
 def log_profiles(log_folder, coords):
     
-    print(log_folder)
-    today = datetime.now()
-    strDate = str(today.date())
-    current_log_file = log_folder + '/' + strDate + ' profiles_log.txt'
+    try:
 
-    
-    if os.path.isfile(current_log_file):
-        f = open(current_log_file, 'a')
-        f.write(str(today) + ',' + str(coords) + '\n')
-        print("ici")
-        f.close()
-    else:
-        print("la")
-        f = open(current_log_file, 'w')
-        f.write(str(today) + ',' + str(coords) + '\n')
-        f.close()
+        today = datetime.now()
+        strDate = str(today.date())
+        current_log_file = log_folder + '/' + strDate + ' profiles_log.txt'
+
+        
+        if os.path.isfile(current_log_file):
+            f = open(current_log_file, 'a')
+            f.write(str(today) + ',' + str(coords) + '\n')
+            f.close()
+        else:
+            f = open(current_log_file, 'w')
+            f.write(str(today) + ',' + str(coords) + '\n')
+            f.close()
+
+    except:
+        return
 
 
     

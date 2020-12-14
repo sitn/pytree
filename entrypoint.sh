6b51b9b9a05d67ae6fadeba99fa7c724b7902d4f
nicolas-sitylb <nbl@ylb.ch>
2020-12-14 11:07:02 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-exec gunicorn --config gunicorn_config.py wsgi:app
+
+echo "########################: ${DEPLOY_ENV}"
+
+if [ "${DEPLOY_ENV}" = "DEV" ];
+then
+    exec python devserver.py
+elif [ "${DEPLOY_ENV}" = "PROD" ];
+then
+    exec gunicorn --config gunicorn_config.py wsgi:app
+else
+    printf "%s %s\n" \
+        "Warning, you have to set the 'DEPLOY_ENV' variable in .env to either" \
+        "'DEV' or 'PROD' before starting."
+fi

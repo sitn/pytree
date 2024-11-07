5ddd8e01d761a036fff6d7a8461aa40142f505f4
Corentin Junod <corentin.junod@ne.ch>
2024-11-07 11:10:43 +0100

    switch to gunicorn

--- start.py
+++ start.py
@@ -1,8 +1,8 @@
-import os
+import os, sys
 from flask_cors import CORS
 from pytree import start_app
-from waitress import serve
 from dotenv import load_dotenv
+from subprocess import run
 load_dotenv()
 
 host = os.getenv('HOST')
@@ -10,8 +10,11 @@ port = os.getenv('PORT')
 app = start_app(os.getenv('CONFIG_FILE'), os.getenv('CPOTREE_EXE'))
 
 if os.getenv('DEPLOY_ENV') == 'PROD' :
+    if os.name == 'nt':
+        print("Staring in production is not supported on Windows")
+        sys.exit()
     print("Starting pytree in production...")
-    serve(app, host=host, port=port, threads=8)
+    run("gunicorn -workers=4 pytree:start_app()")
 else:
     print("Starting pytree in development...")
     CORS(app)

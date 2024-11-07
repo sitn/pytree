e805b5a387837bb6605dffba49759672e350a96d
Corentin Junod <corentin.junod@ne.ch>
2024-11-07 09:22:13 +0100

    Cleanup

--- pytree/__init__.py
+++ pytree/__init__.py
@@ -44,8 +44,7 @@ def start_app(config_file, cpotree_exe):
         headerSize = int.from_bytes(result[0:4], byteorder='little')
         if os.name == 'nt': # Under Windows \n are replaced by \r\n so we need to invert that
             result = bytes(result.replace(b'\r\n', b'\n')) 
-        header_bytes = result[4:4+headerSize]
-        header = json.loads(header_bytes.decode())
+        header = json.loads(result[4:4+headerSize].decode())
         header['headerSize'] = headerSize
         return (result, header)
 
